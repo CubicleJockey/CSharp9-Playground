408ffa9909b38afe4dc97c28e48f140d58d6e784
André Davis <CubicleJockey@users.noreply.github.com>
2020-12-14 13:37:07 -0800

    Getting set up to try out C# 9 features.

---
+++ Playground/Playground/Records.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Playground
+{
+    [TestClass]
+    public class Records
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
